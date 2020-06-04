604688a0e930b5958ed34e52e43133bbf5ed2fa3
Michael Müller <info@ihr-layout.eu>
2020-06-04 11:36:25 +0200

    Update ext_localconf.php
    Dann läuft die Extension auch auf V10

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,23 +5,25 @@ if (!defined('TYPO3_MODE')) {
 
 // Configure frontend plugin
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'JonathanHeilmann.' . $_EXTKEY,
-    'Pi1',
-    array(
-        'Magnificpopup' => 'show'
-    ),
-    array()
+    'JonathanHeilmann.jh_magnificpopup',
+    'Pi1', 
+    [
+            \JonathanHeilmann\jh_magnificpopup\Controller\MagnificpopupController::class =>'show'
+			],
+			[
+            \JonathanHeilmann\jh_magnificpopup\Controller\MagnificpopupController::class => ''
+			]
 );
 
 // Save the IRRE content (use hook to change colPos)
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = \JonathanHeilmann\JhMagnificpopup\Hooks\UpdateColPosHook::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['jh_magnificpopup'] = \JonathanHeilmann\JhMagnificpopup\Hooks\UpdateColPosHook::class;
 
 // Register icon
 $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 $iconRegistry->registerIcon(
     'tx-jhmagnificpopup-pi1',
     \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-    ['source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/ce_wiz.png']
+    ['source' => 'EXT:jh_magnificpopup/Resources/Public/Icons/ce_wiz.png']
 );
 
 // Page TSconfig to add new content element wizard and for global enabled iframe
@@ -38,7 +40,7 @@ $iconRegistry->registerIcon(
 ');
 
 // Add eID for ajax-content
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['jh_magnificpopup_ajax'] = 'EXT:' . $_EXTKEY . '/Resources/Public/Php/EidRunner.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['jh_magnificpopup_ajax'] = 'EXT:jh_magnificpopup/Resources/Public/Php/EidRunner.php';
 
 
 //
